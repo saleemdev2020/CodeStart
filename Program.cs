d8c3b370e98c2f9680bd5375b6aa5e209301b284
saleemdev <77560028+saleemdev2020@users.noreply.github.com>
2021-03-09 23:03:24 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CodeStart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String name = "saleem";
+            int number = 123;
+
+            Console.WriteLine("my name is " + name + number);
+         
+            Console.ReadLine();
+        }
+    }
+}
